56e12c5f5aad29d75c25c29beccd1e3ce848ca0a
Liodakis <mliodakis@travelstaytion.com>
2024-10-31 18:29:36 +0200

    refactor: activities component

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -2,37 +2,42 @@ import { Card, Image, Text, Flex } from '@mantine/core'
 import { weatherActivities } from '../data/weather'
 import { IconHeartFilled } from '@tabler/icons-react'
 import { MyCard } from './common/MyCard.tsx'
+import { useMediaQuery } from '@mantine/hooks'
 
 export function Activities() {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
   return (
-    <MyCard px={48} style={{ maxWidth: 885 }}>
-      <Text size="lg" style={{ display: 'flex', alignItems: 'center' }}>
-        <IconHeartFilled size={24} style={{ marginInlineEnd: 8 }} />
-        Activities in your area
-      </Text>
-      <Flex>
-        {weatherActivities.map((activity, index) => (
-          <Card
-            key={index}
-            bg="transparent"
-            px="12px"
-            ps={index === 0 ? 0 : 12}
-          >
-            <Image
-              src={activity.image}
-              alt={activity.description}
-              fit="cover"
-              radius="10"
-              width={178}
-              height={100}
-              style={{ minWidth: 178, maxHeight: 100 }}
-            />
-            <Text size="12px" mt="xs">
-              {activity.distance} away
-            </Text>
-          </Card>
-        ))}
-      </Flex>
-    </MyCard>
+    isDesktop && (
+      <MyCard px={48} style={{ maxWidth: 892 }}>
+        <Text size="lg" style={{ display: 'flex', alignItems: 'center' }}>
+          <IconHeartFilled size={24} style={{ marginInlineEnd: 8 }} />
+          Activities in your area
+        </Text>
+        <Flex wrap="wrap">
+          {weatherActivities.map((activity, index) => (
+            <Card
+              key={index}
+              bg="transparent"
+              px="12px"
+              ps={index === 0 ? 0 : 12}
+            >
+              <Image
+                src={activity.image}
+                alt={activity.description}
+                fit="cover"
+                radius="10"
+                width={178}
+                height={100}
+                style={{ minWidth: 178, maxHeight: 100 }}
+              />
+              <Text size="12px" mt="xs">
+                {activity.distance} away
+              </Text>
+            </Card>
+          ))}
+        </Flex>
+      </MyCard>
+    )
   )
 }
