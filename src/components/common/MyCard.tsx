2125eaa33436687f27e3fa2f31eec04fbef15ac4
Liodakis <mliodakis@travelstaytion.com>
2024-10-31 18:19:29 +0200

    refactor: update my card props

--- src/components/common/MyCard.tsx
+++ src/components/common/MyCard.tsx
@@ -1,20 +1,24 @@
-import { Card, CardProps } from '@mantine/core'
 import { ReactNode } from 'react'
+import { Card, CardProps } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 
 interface MyCardProps extends CardProps {
   children: ReactNode
 }
 
-export function MyCard({ children, ...props }: MyCardProps) {
+export function MyCard({ children, style, ...props }: MyCardProps) {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
   return (
     <Card
       mb="lg"
+      radius={isDesktop ? 40 : 20}
       style={{
         background: 'rgba(255, 255, 255, 0.1)',
         backdropFilter: 'blur(30px)',
-        borderRadius: '15px',
         padding: '1.5rem',
         boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
+        ...style,
       }}
       {...props}
     >
