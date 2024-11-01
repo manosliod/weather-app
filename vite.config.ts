036f6294b0616296761bdd0336e7726058040bbf
Liodakis <mliodakis@travelstaytion.com>
2024-11-01 08:37:48 +0200

    feat: global paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@data': resolve(__dirname, './src/data'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@providers': resolve(__dirname, './src/providers'),
+      '@components': resolve(__dirname, './src/components'),
+    },
+  },
 })
