285207a5d7f8e81752bb8ed555001c973b0c6a1c
Liodakis <mliodakis@travelstaytion.com>
2024-11-01 07:54:32 +0200

    refactor: add comments

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -6,12 +6,14 @@ import { weatherInfo } from '../data/weather'
 import dayjs from 'dayjs'
 
 export function WeatherInfo() {
+  // Check if the screen width is at least 768px for desktop view
   const isDesktop = useMediaQuery('(min-width: 768px)')
 
   return (
     <Card bg="transparent" py="xl" style={{ minHeight: '308px' }}>
       <Flex direction="row">
         <Flex>
+          {/* Location section */}
           <Text
             size={isDesktop ? '24px' : '18px'}
             style={{
@@ -34,6 +36,7 @@ export function WeatherInfo() {
           align={isDesktop ? 'flex-start' : 'center'}
           style={{ textAlign: isDesktop ? 'left' : 'center' }}
         >
+          {/* Weather type section */}
           <Text
             size={isDesktop ? '48px' : '24px'}
             mt="sm"
@@ -41,7 +44,9 @@ export function WeatherInfo() {
           >
             {weatherInfo.weatherType}
           </Text>
+          {/* Display sun icon for mobile view */}
           {!isDesktop && <FaCloudSun size={180} style={{ marginTop: 12 }} />}
+          {/* Temperature section */}
           <Text
             size={isDesktop ? '64px' : '32px'}
             mt={isDesktop ? 'auto' : 'sm'}
@@ -49,12 +54,14 @@ export function WeatherInfo() {
           >
             {weatherInfo.temperature}°C
           </Text>
+          {/* Date section */}
           <Text size="18px" mt="xs" style={{ fontWeight: 500 }}>
             {dayjs(weatherInfo.date).format('dddd')} |{' '}
             {dayjs(weatherInfo.date).format('DD MMM YYYY')}
           </Text>
         </Flex>
 
+        {/* Display sun icon for desktop view */}
         {isDesktop && (
           <Flex>
             <FaCloudSun size={280} />
